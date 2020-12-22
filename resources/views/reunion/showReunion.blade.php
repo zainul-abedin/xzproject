b4381e6dd60eebc49cabf591cf8ca712f8e61385
Zainul Abedin <mdzainulbd@gmail.com>
2020-12-22 01:09:39 +0100

    Rewrite reunion details

--- resources/views/reunion/showReunion.blade.php
+++ resources/views/reunion/showReunion.blade.php
@@ -9,121 +9,138 @@
     <button type="button" id="list" aria-hidden="true" style="font-size:22px; background-color: white"><i class="fas fa-list"></i></button>
     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>           
 </div>
-<div class="modal-body">
-    <div class="media">
-        <i class="fas fa-heading" style="font-size: 18px"></i>
-        <div class="media-body">
-            <h5 class="mt-0"><span class="ml-2">{{ $reunion->title }}</span></h5>
+<div class="container">
+    <div class="row" style="margin-bottom: 4px">
+        <div class="col-1">
+            <i class="fas fa-heading" style="font-size: 18px"></i>
+        </div>
+        <div class="col-10">
+            <span>{{ $reunion->title }}</span>
         </div>
     </div>
 
-    <div class="media">
-        <i class="fas fa-map-marker-alt" style="font-size: 18px"></i>
-        <div class="media-body">
-            <h5 class="mt-0"><span class="ml-2">{{ $reunion->chantier->location->chantier_adresse->adresse }}</span></h5>
+    <div class="row" style="margin-bottom: 4px">
+        <div class="col-1">
+            <i class="fas fa-map-marker-alt" style="font-size: 18px"></i>
+        </div>
+        <div class="col-10">
+            <span>{{ $reunion->chantier->location->chantier_adresse->adresse }}</span>
         </div>
     </div>
-
 
     @if($reunion->allDay == 0)
     @if((date('d/m/yy', strtotime($reunion->start)))==(date('d/m/yy', strtotime($reunion->end))))
-    <div class="media">
-        <i class="fas fa-clock" style="font-size: 18px"></i>
-        <div class="media-body">
-            <h5 class="mt-0"><span class="ml-2">{{ date('d/m/yy H:i', strtotime($reunion->start)).' ' }} - {{ ' '.date('H:i', strtotime($reunion->end)) }}</span></h5>
+    <div class="row" style="margin-bottom: 4px">
+        <div class="col-1">
+            <i class="fas fa-clock" style="font-size: 18px"></i> 
         </div>
+        <div class="col-10">
+            <span>{{ date('d/m/yy H:i', strtotime($reunion->start)).' ' }} - {{ ' '.date('H:i', strtotime($reunion->end)) }}</span>
+        </div>
+
     </div>
     @else
-    <div class="media">
-        <i class="fas fa-clock" style="font-size: 18px"></i>
-        <div class="media-body">
-            <h5 class="mt-0"><span class="ml-2">{{ date('d/m/yy H:i', strtotime($reunion->start)).' ' }} - {{ ' '.date('d/m/yy H:i', strtotime($reunion->end)) }}</span></h5>
+    <div class="row" style="margin-bottom: 4px">
+        <div class="col-1">
+            <i class="fas fa-clock" style="font-size: 18px"></i>  
+        </div>
+        <div class="col-10">
+            <span>{{ date('d/m/yy H:i', strtotime($reunion->start)).' ' }} - {{ ' '.date('d/m/yy H:i', strtotime($reunion->end)) }}</span> 
         </div>
     </div>
     @endif
-    @endif    
-
+    @endif
 
     @if($reunion->allDay == 1)
-    <div class="media">
-        <i class="fas fa-clock" style="font-size: 18px"></i>
-        <div class="media-body">
-            <h5 class="mt-0"><span class="ml-2">All day</span></h5>
+    <div class="row" style="margin-bottom: 4px">
+        <div class="col-1">
+            <i class="fas fa-clock" style="font-size: 18px"></i>
+        </div>
+        <div class="col-10">
+            <span>All day</span> 
         </div>
     </div>
     @endif
 
-
-
-    @if($reunion->statut === 0)
-    <div class="media">
-        <i class="fas fa-info-circle" style="font-size: 18px"></i>
-        <div class="media-body">
-            <h5 class="mt-0"><span class="ml-2">Réunion n'est pas terminée</span></h5>
+    @if($reunion->statut === 0)  
+    <div class="row" style="margin-bottom: 4px">
+        <div class="col-1">
+            <i class="fas fa-info-circle" style="font-size: 18px"></i> 
+        </div>
+        <div class="col-10">
+            <span>Réunion n'est pas terminée</span> 
         </div>
     </div>
     @elseif($reunion->statut === 1)
-    <div class="media">
-        <i class="fas fa-info-circle" style="font-size: 18px"></i>
-        <div class="media-body">
-            <h5 class="mt-0"><span class="ml-2">Réunion terminée</span></h5>
+    <div class="row" style="margin-bottom: 4px">
+        <div class="col-1">
+            <i class="fas fa-info-circle" style="font-size: 18px"></i> 
+        </div>
+        <div class="col-10">
+            <span>Réunion terminée</span>
         </div>
     </div>
     @endif
 
-
-    <div class="media">
-        <i class="fas fa-feather" style="font-size: 18px"></i>
-        <div class="media-body">
-            <h5 class="mt-0"><span class="ml-2">{{ $reunion->description }}</span></h5>
+    <div class="row" style="margin-bottom: 4px">
+        <div class="col-1">
+            <i class="fas fa-feather" style="font-size: 18px"></i> 
+        </div>
+        <div class="col-10">
+            <span>{{ $reunion->description }}</span>
         </div>
     </div>
 
-    <div class="media" style="margin-bottom: 8px">
-        <i class="fas fa-paperclip" style="font-size: 18px"></i>
-        <div class="media-body">
-            <h5 class="mt-0"><span class="ml-2"></span></h5>
+    <div class="row" style="margin-bottom: 4px"> 
+        <div class="col-1">
+            <i class="fas fa-paperclip" style="font-size: 18px"></i>
+        </div>
+        <div class="col-10">
+            <span></span>
         </div>
     </div>
 
-    <div class="media">
-        <i class="fas fa-user-cog" style="font-size: 18px"></i>
-        <div class="media-body">
-            <h5 class="mt-0"><span class="ml-2">{{ $reunion->createur }}</span></h5>
+    <div class="row" style="margin-bottom: 4px">
+        <div class="col-1">
+            <i class="fas fa-user-cog" style="font-size: 18px"></i>
         </div>
+        <div class="col-10">
+            <span>{{ $reunion->createur }}</span>
+        </div>  
     </div>
+    </br>
 </div>
 
 <script>
-    $(document).ready(function(){
-        
-        $("#trash").click(function(){
+    $(document).ready(function () {
+
+        $("#trash").click(function () {
             alert("En développement");
         });
-        
-        $("#envelope").click(function(){
+
+        $("#envelope").click(function () {
             alert("En développement");
         });
-        
-        $("#camera").click(function(){
+
+        $("#camera").click(function () {
             alert("En développement");
         });
-        
-        $("#paperclip").click(function(){
+
+        $("#paperclip").click(function () {
             alert("En développement");
         });
-        
-        $("#user-shield").click(function(){
+
+        $("#user-shield").click(function () {
             alert("En développement");
         });
-        
-        $("#users").click(function(){
+
+        $("#users").click(function () {
             alert("En développement");
         });
-        
-        $("#list").click(function(){
+
+        $("#list").click(function () {
             alert("En développement");
         });
-        
+
     });
 </script>
